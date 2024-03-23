d1f74441ee5a642d2e7c72f21397247b53ef709d
Roronoa Zoro <shivamsharma8434202092@gmail.com>
2024-03-23 22:26:49 +0530

    attemp to implement cursor take -1

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -23,6 +23,7 @@ const INTERVAL_TIME = 5 * 60 * 100; // 0.5 min or 30sec
 export default function Editor() {
     const [socket, setSocket] = useState(null);
     const [quill, setQuill] = useState(null);
+    const [cursors, setCursors] = useState({})
 
     const { id: documentId } = useParams();
     const saveShortcutRef = useRef(null);
@@ -110,9 +111,6 @@ export default function Editor() {
         [quill, socket]
     );
 
-
-
-
     const wrapperRef = useCallback(wrapper => {
 
         if (wrapper == null) return
@@ -147,6 +145,22 @@ export default function Editor() {
         setQuill(q);
     }, [])
 
+    /*
+        Collaboration
+
+        1. Login User to get Name  → redirect to select document to edit or create new Document 
+        2. On Document’s Document(MonogDb) create a new Field called active User → when the user joins the room just insert the user in to the active user.
+        3. Fetch the activeUser into react state in cursors.
+        4. Display the user based on there range.index from the document.
+    */
+
+    const handleSelectionChange = (range, oldRange, source) => {
+        if (quill == null || socket == null || range == null) return
+
+        const id = Math.random().toString(36).substring(2, 11)
+
+
+    }
 
     return (
         <div id="container" ref={wrapperRef}></div>
